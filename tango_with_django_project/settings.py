5aee4f29406267a0cbe22e8e08c3bac67f44a670
Fraser Dempster <2331229D@student.gla.ac.uk>
2020-02-14 18:28:38 +0000

    chapter 10 done (with errors)

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -122,6 +122,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+SESSION_EXPIRE_AT_BROWSER_VLOSE  = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
